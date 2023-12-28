b5e6b9f72080a5ea2618d98bc3afa9294db6da6f
Steff093 <heinzstefan93@gmail.com>
2023-12-28 23:44:53 +0100

    Update NovelNestDataContext.cs

--- NovelNest.Infrastructure/Database/NovelNestDataContext.cs
+++ NovelNest.Infrastructure/Database/NovelNestDataContext.cs
@@ -5,12 +5,16 @@ namespace NovelNest.Infrastructure.Database
 {
     public class NovelNestDataContext : DbContext
     {
+        public NovelNestDataContext()
+        {
+        }
+
         public NovelNestDataContext(DbContextOptions<NovelNestDataContext> options) { }
         public DbSet<BookEntity> BookEntities { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=GISELA\\SQLEXPRESS;Initial Catalog=NovelNest;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            optionsBuilder.UseSqlServer("");
             base.OnConfiguring(optionsBuilder);
         }
 
