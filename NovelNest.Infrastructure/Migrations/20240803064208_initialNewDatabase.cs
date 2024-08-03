7751f4885cbb2a93ab3fbb44fa54ca2195d3a65e
Steff <skaterkeks@googlemail.com>
2024-08-03 09:04:50 +0200

    work

---
+++ NovelNest.Infrastructure/Migrations/20240803064208_initialNewDatabase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NovelNest.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialNewDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
